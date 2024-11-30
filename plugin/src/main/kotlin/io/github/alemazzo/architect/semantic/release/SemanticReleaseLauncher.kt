92563e29a18600fccfd5065f809fe5be73a1e1a7
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-11-30 15:56:45 +0100

    feat: using PluggableCommand interface to handle arguments

--- plugin/src/main/kotlin/io/github/alemazzo/architect/semantic/release/SemanticReleaseLauncher.kt
+++ plugin/src/main/kotlin/io/github/alemazzo/architect/semantic/release/SemanticReleaseLauncher.kt
@@ -3,13 +3,17 @@ package io.github.alemazzo.architect.semantic.release
 import io.micronaut.configuration.picocli.PicocliRunner
 import jakarta.inject.Singleton
 
+interface MainCommand {
+	fun run(args: List<String>)
+}
+
 @Singleton
-class SemanticReleaseLauncher {
-	fun run(args: Array<String>) {
-		PicocliRunner.run(SemanticReleaseCommand::class.java, *args)
+class SemanticReleaseLauncher: MainCommand {
+	override fun run(args: List<String>) {
+		PicocliRunner.run(SemanticReleaseCommand::class.java, *args.toTypedArray())
 	}
 }
 
 fun main(args: Array<String>) {
-	SemanticReleaseLauncher().run(args)
+	SemanticReleaseLauncher().run(args.toList())
 }
